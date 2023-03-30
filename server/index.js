66b7423511937ae59c737ea478d673b6be2e3b9b
m0rgause <antoresf1@gmail.com>
2023-03-31 06:00:51 +0700

    chore(config.json): add server and client URLs to config file fix(index.js): change default port from 3000 to 5000

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use('/app', (req, res, next) => {
 app.use("*", (req, res) => {
     res.status(404).send({ code: 404, message: "Not found" });
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
