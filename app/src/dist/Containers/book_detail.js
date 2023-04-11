f08456e8671cbf5cc3c85f7cf8e6df46d67f0006
m0rgause <antoresf1@gmail.com>
2023-04-11 22:00:41 +0700

    style(book_detail.js): improve layout and readability of BookDetail component feat(book_detail.js): add conditional rendering for empty title and info fields feat(book_detail.js): add genre buttons to BookDetail component

--- app/src/dist/Containers/book_detail.js
+++ app/src/dist/Containers/book_detail.js
@@ -37,42 +37,57 @@ const BookDetail = () => {
                     </div>
                     <div className="card-body">
                         <div className="row">
-                            <div className="col-md-2 col-12 text-center">
+                            <div className="col-md-3 col-12 text-center">
                                 <img src={result?.thumb} alt={result?.title?.english} width="100%" className="mb-3" />
                                 <a href="asd" className="btn bg-body-tertiary form-control text-white"><i className="bi bi-bookmark"></i> Bookmark</a>
                             </div>
-                            <div className="col-md-10 col-12 mt-3">
-                                <p className="text-muted lh-1 word-break">{result?.title?.full}</p>
+                            <div className="col-md-9 col-12 mt-3">
+                                {result?.title?.full !== '' ?
+                                    <p className="text-muted lh-1 word-break">{result?.title?.full}</p>
+                                    : null}
                                 <p className="text-muted" style={{ fontSize: "14px" }}>{result?.description}</p>
                                 <div className="d-flex justify-content-md-start justify-content-evenly my-3">
                                     <a href={result?.chapters?.first?.path} className="btn bg-body-tertiary text-white mx-1"><font className="text-muted">First: </font>{result?.chapters?.first?.ch}</a>
                                     <a href={result?.chapters?.last?.path} className="btn bg-body-tertiary text-white mx-1"><font className="text-muted">Last: </font>{result?.chapters?.last?.ch}</a>
                                 </div>
-                                <div className="info">
-                                    <span>
-                                        <b>Status : </b>
-                                        {result?.info?.status}
-                                    </span>
-                                    <span>
-                                        <b>Released : </b>
-                                        {result?.info?.released}
-                                    </span>
-                                    <span>
-                                        <b>Artist : </b>
-                                        {result?.info?.artis}
-                                    </span>
-                                    <span>
-                                        <b>Type : </b>
-                                        {result?.info?.type}
-                                    </span>
-                                    <span>
-                                        <b>Author : </b>
-                                        {result?.info?.author}
-                                    </span>
-                                    <span>
-                                        <b>postedOn : </b>
-                                        {result?.info?.postedOn}
-                                    </span>
+                                <div className="info d-flex flex-column ">
+                                    {result?.info?.status !== '' ?
+                                        <span className="border p-2">
+                                            <b>Status : </b>
+                                            {result?.info?.status}
+                                        </span>
+                                        : null}
+                                    {result?.info?.released !== '' ?
+                                        <span className="border p-2">
+                                            <b>Released : </b>
+                                            {result?.info?.released}
+                                        </span>
+                                        : null}
+                                    {result?.info?.artis !== '' ?
+                                        <span className="border p-2">
+                                            <b>Artist : </b>
+                                            {result?.info?.artis}
+                                        </span> : null}
+                                    {result?.info?.type !== '' ?
+                                        <span className="border p-2">
+                                            <b>Type : </b>
+                                            {result?.info?.type}
+                                        </span> : null}
+                                    {result?.info?.author !== '' ?
+                                        <span className="border p-2">
+                                            <b>Author : </b>
+                                            {result?.info?.author}
+                                        </span> : null}
+                                    {result?.info?.postedOn !== '' ?
+                                        <span className="border p-2">
+                                            <b>postedOn : </b>
+                                            {result?.info?.postedOn}
+                                        </span> : null}
+                                </div>
+                                <div className="genre mt-3">
+                                    {result?.genres?.map((genre, index) => (
+                                        <a href={genre?.path} className="btn bg-body-tertiary text-white mx-1 my-1" key={index}>{genre?.tag}</a>
+                                    ))}
                                 </div>
                             </div>
                         </div>
