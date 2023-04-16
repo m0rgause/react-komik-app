d0301cedb7865988858534281f2dfae216e3c97f
m0rgause <antoresf1@gmail.com>
2023-04-16 16:30:40 +0700

    feat(book_reader.js): add chapter navigation buttons feat(book_reader.js): add progress bar for reading progress fix(style.css): add style for progress bar fix(image_loader.js): add key prop to ImageLoader component fix(image_loader.js): add center tag to loader image in ImageLoader component

--- app/src/dist/Components/image_loader.js
+++ app/src/dist/Components/image_loader.js
@@ -5,12 +5,15 @@ import LazyLoad from "react-lazy-load";
 const ImageLoader = (props) => {
     return (
         <LazyLoad>
-            <Img
+            <Img key={props.index}
                 src={"https://cdn.statically.io/img/" + props.src.replace('https://', "")}
                 alt={props.alt}
                 className={props.className}
+                id={props.index}
                 loader={
-                    <img alt="loading" src={require("../img/loader.svg").default} />
+                    <center>
+                        <img alt="loading" src={require("../img/loader.svg").default} />
+                    </center>
                 }
                 style={{ width: "100%" }} />
         </LazyLoad>
--- app/src/dist/Containers/book_reader.js
+++ app/src/dist/Containers/book_reader.js
@@ -2,31 +2,93 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import axios from "axios";
-import ImageLoader from "../Components/image_loader";
 import PageLoader from "../Components/page_loader";
+import ImageLoader from "../Components/image_loader";
+
+const ChapterButtons = ({ book }) => {
+    return (
+        <div className="d-flex justify-content-end my-3">
+            <a
+                href={book?.prevChapter}
+                className={`btn bg-body-tertiary text-white bgH me-2 ${!book?.prevChapter && "disabled"
+                    }`}
+            >
+                &lt; Prev Chapter
+            </a>
+            <a
+                href={book?.nextChapter}
+                className={`btn bg-body-tertiary text-white bgH ${!book?.nextChapter && "disabled"
+                    }`}
+            >
+                Next Chapter &gt;
+            </a>
+        </div>
+    );
+};
 
 const BookReader = () => {
     const { path } = useParams();
-    const [book, setBook] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [book, setBook] = useState(null);
 
     useEffect(() => {
-        axios.get(`https://api.kikii.me/api/book/${path}`)
+        let isMounted = true;
+        setIsLoading(true);
+        axios
+            .get(`https://api.kikii.me/api/book/${path}`)
             .then((response) => {
-                setBook(response.data.result);
-                setIsLoading(false);
+                if (isMounted) {
+                    setBook(response.data.result);
+                    setIsLoading(false);
+                }
             })
-            .catch((error) => { console.log(error); setIsLoading(false) });
-    }, [])
+            .catch((error) => {
+                console.log(error);
+                setIsLoading(false);
+            });
+        return () => {
+            isMounted = false;
+        };
+    }, [path])
+
 
     if (isLoading) {
-        return <PageLoader />
+        return <PageLoader />;
     }
+
+
 
     return (
         <HelmetProvider>
-            <Helmet><title>{book?.title}</title></Helmet>
+            <Helmet>
+                <title>{book?.title}</title>
+            </Helmet>
+            <div className="container mt-5">
+                <div className="title text-center">
+                    <h4>{book?.title}</h4>
+                </div>
+            </div>
+            <div
+                id="reader"
+                className="d-flex flex-column "
+                style={{
+                    maxWidth: "1142px",
+                    margin: "10px auto",
+                }}
+            >
+                <ChapterButtons book={book} />
+                {book?.pages[0]?.images.map((el, index) => (
+                    <ImageLoader key={index} src={el} index={index} alt={book?.title} />
+                ))}
+                <ChapterButtons book={book} />
+            </div>
+            <div className="ch-progress-bar sticky-bottom">
+                <div className="bar">
+                    <div className="bar-long"></div>
+                </div>
+            </div>
         </HelmetProvider>
-    )
-}
-export default BookReader;
+    );
+};
+
+export default React.memo(BookReader);
