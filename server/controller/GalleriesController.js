bc011138d3dbdf584ee64b50d9317f9d59fa3af8
m0rgause <antoresf1@gmail.com>
2023-04-26 04:15:28 +0700

    feat(GalleriesController.js): add query parameters for filtering results by status, order, and type refactor(GalleriesController.js): extract book parsing logic into a separate function called getBooks()

--- server/controller/GalleriesController.js
+++ server/controller/GalleriesController.js
@@ -1,53 +1,42 @@
 const { default: Axios } = require('axios');
 const cheerio = require('cheerio');
 const { uri } = require('../config.json');
-const { _replace } = require('../utils/func.js');
+const { _replace, getBooks } = require('../utils/func.js');
 
 const gallery = async (req, res) => {
     try {
         let page = req.params.page ? req.params.page : 1;
         let url = req.params.uri;
-
-        let response
+        let { status, order, type } = req.query;
+        // [status = [ongoing, completed, hiatus,all]]
+        status = (status && status != "all") ? status : "";
+        // [order = [default, title, titlereverse, update, latest, popular]]
+        order = (order && order != "default") ? order : "";
+        // [type = [all, manga, manhwa, manhua, comic, novel]]
+        type = (type && type != "all") ? type : "";
+        let response;
         if (url == "manga") {
-            response = await Axios.get(`${uri}/manga/page/${page}`)
+            response = await Axios.get(`${uri}/manga?page=${page}&status=${status}&order=${order}`);
         } else if (url == "project") {
-            response = await Axios.get(`${uri}/project/page/${page}`)
-        } else if (url == "new-release") {
-            response = await Axios.get(`${uri}/manga/page/${page}?order=update`)
+            response = await Axios.get(`${uri}/project/page/${page}`);
         } else {
             return res.status(500).send({
                 code: 500,
                 message: "Bad request"
-            })
+            });
         }
 
         $ = cheerio.load(response.data);
-
-        const books = $('div.listupd > div.bs');
-        let book = []
-        books.each((i, el) => {
-            book.push({
-                title: $(el).find("div.bsx > a").attr('title'),
-                path: _replace($(el).find("div.bsx > a").attr("href")),
-                type: $(el).find('div.bsx > a').eq(0).find('div.limit > span[class*="type"]').attr('class').replace("type", ''),
-                status: ($(el).find('div.bsx > a').eq(0).find('div.limit > span[class*="status"]').text() !== "") ? "Completed" : "Ongoing",
-                thumb: $(el).find('div.bsx > a').eq(0).find("img").attr('src'),
-                chapter: $(el).find('div.bsx > a > div.bigor > div.adds > div.epxs ').text(),
-                rating: Number($(el).find('div.bsx > a > div.bigor > div.adds > div.rt > div.rating > div.numscore ').text()),
-            })
-        })
-        return res.status(200).send({ code: 200, message: "Succesfully", page: Number(page), result: book });
+        return res.status(200).send({ code: 200, message: "Succesfully", page: Number(page), result: getBooks($('div.listupd > div.bs')) });
     } catch (error) {
-        console.log([error.code, error.request.res.statusCode, error.config.url])
+        console.log([error.code, error.request.res.statusCode, error.config.url]);
         return res.status(500).send({
             code: 500,
             message: "Bad request"
-        })
+        });
     }
-
-}
+};
 
 module.exports = {
     gallery
-}
+};
