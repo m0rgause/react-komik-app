ab20c7794aaf40a2d512fd2de046ca05fe43ca34
m0rgause <antoresf1@gmail.com>
2023-04-29 17:59:25 +0700

    style(history.js): reformat code and add card layout to history component.

--- app/src/dist/Containers/history.js
+++ app/src/dist/Containers/history.js
@@ -34,10 +34,46 @@ const History = () => {
 
     return (
         <div className="container">
-            <OwlCarousel className='owl-theme' {...options}>
-
-            </OwlCarousel>
-        </div>
+            <div className="card mt-5 shadow">
+                <div className="card-header">
+                    <div className="d-flex">
+                        <div style={{
+                            backgroundColor: "var(--bs-secondary-bg)",
+                            marginBottom: "-10px"
+                        }} className="px-3 pt-2 rounded-top">
+                            <h6 className="card-title">History</h6>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-body">
+                    <div className="">
+                        <OwlCarousel className='owl-theme' {...options}>
+                            {history.length > 0 ? history.slice(0, 9).map((item, index) => {
+                                const book = JSON.parse(localStorage.getItem(item)) || [];
+                                return (
+                                    <div key={index}>
+                                        <div className="px-2 my-2">
+                                            <a href={book.path} className="popularHover">
+                                                <div className="card popular">
+                                                    <div className="thumb" >
+                                                        <ImageLoader src={book.thumb} alt={book.title} />
+                                                    </div>
+                                                    <div className="card-body">
+                                                        <h6 className="card-title title fs-6 lh-1 word-break" style={{
+                                                            color: "var(--bs-emphasis-color)"
+                                                        }}>{book.title}</h6>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                )
+                            }) : <div></div>}
+                        </OwlCarousel>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 export default History;
